9bdff0e0ef101ad03241068aae1e8547bca8d784
ivg97 <gavr97.97@mail.ru>
2024-04-29 00:41:39 +0300

    Edit allowed hosts

--- green_gear/green_gear/settings.py
+++ green_gear/green_gear/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(lql1yrcq4wh4m48cvx&pts%z2mxvylbamgpi6taz34ydv)tj#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
