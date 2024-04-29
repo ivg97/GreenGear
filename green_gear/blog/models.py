2b3b44293002ac8e20e91761f8b69f1e22052873
ivg97 <gavr97.97@mail.ru>
2024-04-29 19:10:54 +0300

    add models and migrations

--- green_gear/blog/models.py
+++ green_gear/blog/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Model for table blog_posts"""
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Title',
+    )
+    text = models.TextField(
+        verbose_name='Text',
+        blank=True,
+        null=True,
+        max_length=5000
+    )
+    slug = models.SlugField()
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        verbose_name='Author',
+        related_name='posts',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created at',
+    )
+    image = models.ImageField(
+        upload_to='images/posts/%Y/',
+        blank=True,
+        null=True,
+        verbose_name='Image',
+    )
+    category = models.ForeignKey(
+        'blog.Category',
+        on_delete=models.CASCADE,
+        verbose_name='Category',
+        related_name='posts',
+        blank=True,
+        null=True,
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name='Active',
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['-created_at']
+        db_table = 'blog_posts'
+
+
+class Category(models.Model):
+    """Model for table blog_categories"""
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Title',
+    )
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['title']
+        db_table = 'blog_categories'
--- green_gear/contact/models.py
+++ green_gear/contact/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class CommunicationService(models.Model):
+    """Model for table communication_services."""
+    subject = models.CharField(
+        max_length=255,
+        verbose_name='Subject',
+    )
+    email = models.EmailField(
+        verbose_name='Email',
+    )
+    message = models.TextField(
+        verbose_name='Message',
+        max_length=1000,
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name='Active',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created at',
+    )
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'Communication Service'
+        verbose_name_plural = 'Communication Services'
+        ordering = ['-created_at']
+        db_table = 'communication_service'
+
