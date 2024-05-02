8e63a33c0db59fb14bd12496b0635c0bb7492aaf
Comrade <59848910+artemka201601@users.noreply.github.com>
2024-05-02 11:56:32 +0300

    адаптация для телефона about us, blog, contact service,

--- green_gear/static/js/script.js
+++ green_gear/static/js/script.js
@@ -1,17 +1,19 @@
 /*начало блока nav*/
-document.getElementById('show_close_menu').addEventListener("click", function() {
+document.getElementById('show_menu').addEventListener("click", function() {
 	var nav=document.getElementById("nav");
-	var img=document.getElementById("show_close_menu");
-	if (nav.style.height=="100%"){
-		nav.style.height="72px";
-		img.setAttribute("src", "/static/img/chevron_down_menu.svg");
-		img.style.top="30px";
-		img.style.right="30px";
-	} else {
-		nav.style.height="100%";
-		img.setAttribute("src", "/static/img/close_menu.svg");
-		img.style.top="11px";
-		img.style.right="11px";
-	}
+	var img_show=document.getElementById("show_menu");
+	var img_close=document.getElementById("close_menu");
+	nav.style.height="100%";
+	img_show.style.display="none";
+	img_close.style.display="block";
+})
+
+document.getElementById('close_menu').addEventListener("click", function() {
+	var nav=document.getElementById("nav");
+	var img_show=document.getElementById("show_menu");
+	var img_close=document.getElementById("close_menu");
+	nav.style.height="72px";
+	img_show.style.display="block";
+	img_close.style.display="none";
 })
 /*конец блока nav*/
