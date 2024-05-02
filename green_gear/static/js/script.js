26b044528ab20c7bab657dd45eed096f93204cb3
Comrade <59848910+artemka201601@users.noreply.github.com>
2024-05-02 23:09:09 +0300

    адаптация+js

--- green_gear/static/js/script.js
+++ green_gear/static/js/script.js
@@ -17,3 +17,73 @@ document.getElementById('close_menu').addEventListener("click", function() {
 	img_close.style.display="none";
 })
 /*конец блока nav*/
+
+/*начало блока footer*/
+/*конец блока footer*/
+
+/*начало блока home*/
+/*конец блока home*/
+
+/*начало блока about ux*/
+/*конец блока about ux*/
+
+/*начало блока services*/
+/*конец блока services*/
+
+/*начало блока list_page*/
+try {
+	var list_page_view_all=document.getElementById("list_page_view_all");
+	var list_page_ai_insights=document.getElementById("list_page_ai_insights");
+	var list_page_automation=document.getElementById("list_page_automation");
+	var list_page_industry_trends=document.getElementById("list_page_industry_trends");
+	var list_page_innovation=document.getElementById("list_page_innovation");
+
+	document.getElementById('list_page_view_all').addEventListener("click", function() {
+		list_page_view_all.style.border="solid 1px #000000";
+		list_page_ai_insights.style.border="none";
+		list_page_automation.style.border="none";
+		list_page_industry_trends.style.border="none";
+		list_page_innovation.style.border="none";
+	})
+
+	document.getElementById('list_page_ai_insights').addEventListener("click", function() {
+		list_page_view_all.style.border="none";
+		list_page_ai_insights.style.border="solid 1px #000000";
+		list_page_automation.style.border="none";
+		list_page_industry_trends.style.border="none";
+		list_page_innovation.style.border="none";
+	})
+
+	document.getElementById('list_page_automation').addEventListener("click", function() {
+		list_page_view_all.style.border="none";
+		list_page_ai_insights.style.border="none";
+		list_page_automation.style.border="solid 1px #000000";
+		list_page_industry_trends.style.border="none";
+		list_page_innovation.style.border="none";
+	})
+
+	document.getElementById('list_page_industry_trends').addEventListener("click", function() {
+		list_page_view_all.style.border="none";
+		list_page_ai_insights.style.border="none";
+		list_page_automation.style.border="none";
+		list_page_industry_trends.style.border="solid 1px #000000";
+		list_page_innovation.style.border="none";
+	})
+
+	document.getElementById('list_page_innovation').addEventListener("click", function() {
+		list_page_view_all.style.border="none";
+		list_page_ai_insights.style.border="none";
+		list_page_automation.style.border="none";
+		list_page_industry_trends.style.border="none";
+		list_page_innovation.style.border="solid 1px #000000";	
+	})
+} catch (TypeError) {}
+
+
+/*конец блока list_page*/
+
+/*начало блока detail_page*/
+/*конец блока detail_page*/
+
+/*начало блока contact*/
+/*конец блока contact*/
